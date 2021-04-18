557b767230bdd0b3742f4a46fba3bf7fdf31658f
Divineife <faleyedi01@gmail.com>
2021-04-18 18:21:08 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const https = require("https");
+const fs = require('fs');
+const http = require("http");
+
+
+let url ="https://jsonplaceholder.typicode.com/posts";
+https.get(url, res=> {
+
+    let body ='';
+
+    res.on('data', data => {
+        body += data;
+    })
+    res.on('end', ()=> fs.writeFile("./result/posts.json",body,err => {
+        if(err) throw err;
+        console.log("written")
+    } ));
+})
